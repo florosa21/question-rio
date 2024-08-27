fe01e51331eaf8467f0d081ea92ed842bae82593
Kelly <101299721+florosa21@users.noreply.github.com>
2024-08-27 10:57:47 -0300

    criando

--- main.js
+++ main.js
@@ -3,3 +3,25 @@ const caixaPergunta = document.querySelector(".caixa-pergunta");
 const caixaAlternativa = document.querySelector(".caixa-alternativa");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+
+
+const pergunta = [
+   {
+          enunciado:"Pergunta 1",
+          alternativa: ["alternativa1", "alternativa2", "alternativa3"]
+   },
+   {
+           enunciado:"Pergunta 2",
+          alternativa: ["alternativa1", "alternativa2", "alternativa3"]
+   },
+   {
+           enunciado:"Pergunta 3",
+           alternativa: ["alternativa1", "alternativa2", "alternativa3"]
+   },
+   {
+           enunciado:"Pergunta 4",
+           alternativa: ["alternativa1", "alternativa2", "alternativa3"]
+  },
+
+
+];
